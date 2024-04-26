c41df24a077011ceea51306751298977376a9439
MeetPatel012 <companywork012@gmail.com>
2024-04-26 14:29:00 +0530

    Color Changed

--- src/app/components/sidebar/sidebar.tsx
+++ src/app/components/sidebar/sidebar.tsx
@@ -22,7 +22,7 @@ export default function Sidebar() {
           </li>
 
           <li
-            className="p-5 list-none hover:text-blue-400 hover:bg-slate-100 flex justify-left items-center hover:ml-1 cursor-pointer  "
+            className="p-5 list-none hover:text-green-500  hover:bg-slate-100 flex justify-left items-center hover:ml-1 cursor-pointer  "
             onClick={() => navi("/planned")}
           >
             <img src="./add-contact.png" className="w-4 h-auto mr-6" />
@@ -30,7 +30,7 @@ export default function Sidebar() {
           </li>
 
           <li
-            className="p-5 list-none hover:text-green-500 hover:bg-slate-100 flex justify-left items-center hover:ml-1 cursor-pointer "
+            className="p-5 list-none hover:text-blue-400  hover:bg-slate-100 flex justify-left items-center hover:ml-1 cursor-pointer "
             onClick={() => navi("/assigned")}
           >
             <img src="./clipboard.png" className="w-4 h-auto mr-6" />
